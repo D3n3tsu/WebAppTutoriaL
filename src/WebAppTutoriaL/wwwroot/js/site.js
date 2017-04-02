36511c92ba8514b17b92af5893f5501bf39411ed
D3n3tsu <ddm_09@ukr.net>
2017-04-02 21:43:49 +0300

    bober file added with jquery and underscore

--- src/WebAppTutoriaL/wwwroot/js/site.js
+++ src/WebAppTutoriaL/wwwroot/js/site.js
@@ -1,13 +1,14 @@
 ﻿//site.js
+(function () {
+    var ele = document.getElementById('username');
+    ele.innerHTML = "Shawn Wildermuth";
 
-var ele = document.getElementById('username');
-ele.innerHTML = "Shawn Wildermuth";
+    var main = document.getElementById('main');
 
-var main = document.getElementById('main');
-
-main.onmouseenter = function () {
-    main.style = "background-color: #888;";
-}
-main.onmouseleave = function () {
-    main.style = "";
-}
+    main.onmouseenter = function () {
+        main.style = "background-color: #888;";
+    }
+    main.onmouseleave = function () {
+        main.style = "";
+    }
+})();
