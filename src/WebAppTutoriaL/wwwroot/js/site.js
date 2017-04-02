a8802080efaaa0a2f9d6e845b61a4a0c3952993f
D3n3tsu <ddm_09@ukr.net>
2017-04-02 23:17:05 +0300

    SidebarToggle option added

--- src/WebAppTutoriaL/wwwroot/js/site.js
+++ src/WebAppTutoriaL/wwwroot/js/site.js
@@ -1,21 +1,33 @@
 ﻿/// <reference path="../lib/jquery/dist/jquery.min.js" />
 //site.js
 (function () {
-    var ele = $('#username');
-    ele.text = "Shawn Wildermuth";
+    //var ele = $('#username');
+    //ele.text = "Shawn Wildermuth";
 
-    var main = $('#main');
+    //var main = $('#main');
 
-    main.on('mouseenter', function () {
-        main.style = "background-color: #888;";
-    });
-    main.on('mouseleave', function () {
-        main.style = "";
-    });
+    //main.on('mouseenter', function () {
+    //    main.style = "background-color: #888;";
+    //});
+    //main.on('mouseleave', function () {
+    //    main.style = "";
+    //});
 
-    var menuitems = $('ul.menu li a');
-    menuitems.on('click', function () {
-        var me = $(this);
-        alert(me.text());
-    });
+    //var menuitems = $('ul.menu li a');
+    //menuitems.on('click', function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //});
+
+
+    var $sidebarandwrapper = $('#sidebar,#wrapper');
+
+    $('#sidebarToggle').on('click', function () {
+        $sidebarandwrapper.toggleClass('hide-sidebar');
+        if ($sidebarandwrapper.hasClass('hide-sidebar')) {
+            $(this).text('Show sidebar');
+        } else {
+            $(this).text('Hide sidebar');
+        }
+    })
 })();
