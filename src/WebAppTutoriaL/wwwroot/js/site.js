74881b766c15b6b18bce6dca74cf566353eb5fbc
D3n3tsu <ddm_09@ukr.net>
2017-04-02 21:26:55 +0300

    mouseenter and mouseleave events added

--- src/WebAppTutoriaL/wwwroot/js/site.js
+++ src/WebAppTutoriaL/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 
 var ele = document.getElementById('username');
 ele.innerHTML = "Shawn Wildermuth";
+
+var main = document.getElementById('main');
+
+main.onmouseenter = function () {
+    main.style = "background-color: #888;";
+}
+main.onmouseleave = function () {
+    main.style = "";
+}
