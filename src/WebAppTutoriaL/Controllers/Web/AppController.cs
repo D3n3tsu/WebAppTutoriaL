6d168eaa13e8eadd1fc11f6389e9afde0679c999
D3n3tsu <d3n3tsu@gmail.com>
2017-04-03 22:46:43 +0300

    Controller and App view added. MVS enabled.

---
+++ src/WebAppTutoriaL/Controllers/Web/AppController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Webtutorial.Controllers.Web
+{
+    public class AppController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
