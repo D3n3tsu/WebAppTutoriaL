d6e97aa5e97d02e789e6f6067ead25477c6df44f
D3n3tsu <ddm_09@ukr.net>
2017-04-10 23:47:56 +0300

    API controller created

---
+++ src/WebAppTutoriaL/Controllers/Api/TripController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTutoriaL.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+            return Json(new { Name = "Shawn" });
+        }
+    }
+}
