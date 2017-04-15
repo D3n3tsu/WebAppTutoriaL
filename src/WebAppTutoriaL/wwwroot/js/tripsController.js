d5933da20fba9ad14bbe58d5ebdcd818564c97a2
D3n3tsu <ddm_09@ukr.net>
2017-04-15 18:58:53 +0300

    angular lib added.app-trips angular module added with tripsController.

---
+++ src/WebAppTutoriaL/wwwroot/js/tripsController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    "use strict";
+    angular.module("app-trips")
+    .controller("tripsController", tripsController);
+
+    function tripsController() {
+        var vm = this;
+
+        vm.trips = [{
+            name: "one trip",
+            created: new Date
+        },
+        {
+            name: "second trip",
+            created: new Date
+        }];
+    }
+})();
