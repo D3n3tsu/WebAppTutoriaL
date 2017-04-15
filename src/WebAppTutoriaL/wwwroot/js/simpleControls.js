333c9996799297fc112788328370e04389b46077
D3n3tsu <ddm_09@ukr.net>
2017-04-15 22:04:17 +0300

    waitCursor directive added to show while data is loading.

---
+++ src/WebAppTutoriaL/wwwroot/js/simpleControls.js
@@ -0,0 +1,17 @@
+﻿
+(function () {
+"use strict";
+
+angular.module("simpleControls", [])
+    .directive("waitCursor", waitCursor);
+
+function waitCursor() {
+    return {
+        restrict: "E",
+        scope: {
+            show: "=displayWhen"
+        },
+        templateUrl: "/views/waitCursor.html"
+    };
+}
+})();
