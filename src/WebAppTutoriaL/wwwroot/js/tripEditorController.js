2b5d70471878fbfe43e16d75439f1541572887c5
D3n3tsu <d3n3tsu@gmail.com>
2017-04-16 11:46:43 +0300

    TripEditorView now adds stops to trips edited. Bug fixed when sometimes undefined stop number 1(not 0) was shown first.

--- src/WebAppTutoriaL/wwwroot/js/tripEditorController.js
+++ src/WebAppTutoriaL/wwwroot/js/tripEditorController.js
@@ -7,12 +7,14 @@
     function tripEditorController($routeParams, $http) {
         var vm = this;
         vm.tripName = $routeParams.tripName;
+        vm.newStop = {};
 
         vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+        $http.get(url)
         .then(function (response) {
             //Success
             angular.copy(response.data, vm.stops);
@@ -23,6 +25,24 @@
         }).finally(function () {
             vm.isBusy = false;
         });
+
+        vm.addStop = function () {
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (responce) {
+                    //Success
+                    vm.stops.push(responce.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function (err) {
+                    //Failure
+                    vm.errprMessage = "Failed to add new stop. " + err;
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        };
     }
 
     function _showMap(stops) {
@@ -32,14 +52,14 @@
                 return {
                     lat: item.latitude,
                     long: item.longitude,
-                    info: item.name
+                    info: item.name + " - " + item.arrival
                 };
             });
             //Show map
             travelMap.createMap({
                 stops: mapStops,
                 selector: "#map",
-                currentStop: 1,
+                currentStop: 0,
                 initialZoom:3
             });
         }
